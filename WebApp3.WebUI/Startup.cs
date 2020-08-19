bcca23052af3094884f1e67c330554498612eca9
Ivan Rainbolt <7623933+IvanRainbolt@users.noreply.github.com>
2020-08-19 12:23:57 -0400

    razor pages added to MVC app.

--- WebApp3.WebUI/Startup.cs
+++ WebApp3.WebUI/Startup.cs
@@ -24,6 +24,8 @@ namespace WebApp3.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,7 @@ namespace WebApp3.WebUI
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
